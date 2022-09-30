6f7a97c53d9c0a7a86e5e7890d728a6441a0299b
Dohyeon Lee <dlehgus1110@naver.com>
2022-09-30 20:42:06 +0900

    feat(constants): 상수 파일 및 상수 추가
    DOM, 게임 조건, 메세지에 쓰일 상수를 생성하고
    용도별로 파일을 나누었습니다.

---
+++ src/constants/dom.js
@@ -0,0 +1,6 @@
+const $USER_INPUT = document.querySelector('#user-input')
+const $SUBMIT_BUTTON = document.querySelector('#submit')
+const $RESULT = document.querySelector('#result')
+const $RESTART_BUTTON = document.querySelector('#game-restart-button')
+
+export { $USER_INPUT, $SUBMIT_BUTTON, $RESULT, $RESTART_BUTTON }
---
+++ src/constants/message.js
@@ -0,0 +1,20 @@
+const MSG_ERROR_NOT_A_NUM = `입력에 숫자 이외의 문자가 있습니다. 다시 입력 해 주세요!`
+const MSG_ERROR_LENGTH = `${MAX_NUMBER_LENGTH}자리의 숫자를 입력해 주세요!`
+const MSG_ERROR_REPEATED_NUM = `입력에 중복된 숫자가 있습니다. 다시 입력해 주세요!`
+const MSG_ERROR_RANGE = `입력 가능한 숫자 범위는 ${MIN_NUMBER_RANGE} ~ ${MAX_NUMBER_RANGE} 입니다.`
+
+const MSG_GAME_STLIKE = '스트라이크'
+const MSG_GAME_BALL = '볼'
+const MSG_GAME_NOTHING = '낫싱'
+const MSG_GAME_WIN = '⚾정답을 맞추셨습니다⚾'
+
+export {
+	MSG_ERROR_NOT_A_NUM,
+	MSG_ERROR_LENGTH,
+	MSG_ERROR_REPEATED_NUM,
+	MSG_ERROR_RANGE,
+	MSG_GAME_STLIKE,
+	MSG_GAME_BALL,
+	MSG_GAME_NOTHING,
+	MSG_GAME_WIN,
+}
