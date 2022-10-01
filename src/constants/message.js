5bbd224a484d7fcda4dc272d1d424fb83f05f6ec
Dohyeon Lee <dlehgus1110@naver.com>
2022-10-01 23:25:04 +0900

    chore(message.js): 메세지 상수 변경 및 그룹화
    상수를 용도별 객체로 그룹화하고 메세지 일부를 수정하였습니다.

--- src/constants/message.js
+++ src/constants/message.js
@@ -1,20 +1,14 @@
-const MSG_ERROR_NOT_A_NUM = `입력에 숫자 이외의 문자가 있습니다. 다시 입력 해 주세요!`
-const MSG_ERROR_LENGTH = `${MAX_NUMBER_LENGTH}자리의 숫자를 입력해 주세요!`
-const MSG_ERROR_REPEATED_NUM = `입력에 중복된 숫자가 있습니다. 다시 입력해 주세요!`
-const MSG_ERROR_RANGE = `입력 가능한 숫자 범위는 ${MIN_NUMBER_RANGE} ~ ${MAX_NUMBER_RANGE} 입니다.`
-
-const MSG_GAME_STLIKE = '스트라이크'
-const MSG_GAME_BALL = '볼'
-const MSG_GAME_NOTHING = '낫싱'
-const MSG_GAME_WIN = '⚾정답을 맞추셨습니다⚾'
-
-export {
-	MSG_ERROR_NOT_A_NUM,
-	MSG_ERROR_LENGTH,
-	MSG_ERROR_REPEATED_NUM,
-	MSG_ERROR_RANGE,
-	MSG_GAME_STLIKE,
-	MSG_GAME_BALL,
-	MSG_GAME_NOTHING,
-	MSG_GAME_WIN,
+const errorMessages = {
+	typeError: `입력에 숫자 이외의 문자가 있습니다. 다시 입력해 주세요!`,
+	lengthError: `숫자의 길이가 올바르지 않습니다. 다시 입력해 주세요`,
+	repeatedNumberError: `입력에 중복된 숫자가 있습니다. 다시 입력해 주세요!`,
+	rangeError: `입력 가능한 숫자 범위를 벗어났습니다. 다시 입력해 주세요!`,
 }
+const gameMessages = {
+	strike: '스트라이크',
+	ball: '볼',
+	nothing: '낫싱',
+	win: '⚾정답을 맞추셨습니다⚾',
+}
+
+export { errorMessages, gameMessages }
