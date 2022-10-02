21a677866f85f6472aba08426b80f431c97016dd
Dohyeon Lee <dlehgus1110@naver.com>
2022-10-02 13:31:20 +0900

    chore(message.js): 상수명 스네이크 케이스로 수정

--- src/constants/message.js
+++ src/constants/message.js
@@ -1,14 +1,14 @@
-const errorMessages = {
-	typeError: `입력에 숫자 이외의 문자가 있습니다. 다시 입력해 주세요!`,
-	lengthError: `숫자의 길이가 올바르지 않습니다. 다시 입력해 주세요`,
-	repeatedNumberError: `입력에 중복된 숫자가 있습니다. 다시 입력해 주세요!`,
-	rangeError: `입력 가능한 숫자 범위를 벗어났습니다. 다시 입력해 주세요!`,
+const ERROR_MSG = {
+	TYPE_ERR: `입력에 숫자 이외의 문자가 있습니다. 다시 입력해 주세요!`,
+	LENGTH_ERR: `숫자의 길이가 올바르지 않습니다. 다시 입력해 주세요`,
+	REPEATED_NUM_ERR: `입력에 중복된 숫자가 있습니다. 다시 입력해 주세요!`,
+	RANGE_ERR: `입력 가능한 숫자 범위를 벗어났습니다. 다시 입력해 주세요!`,
 }
-const gameMessages = {
-	strike: '스트라이크',
-	ball: '볼',
-	nothing: '낫싱',
-	win: '⚾정답을 맞추셨습니다⚾',
+const GAME_MSG = {
+	STLIKE: '스트라이크',
+	BALL: '볼',
+	NOTHING: '낫싱',
+	WIN: '⚾정답을 맞추셨습니다⚾',
 }
 
-export { errorMessages, gameMessages }
+export { ERROR_MSG, GAME_MSG }
