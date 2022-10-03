f3039bd088627cc24b68e54322b935d1b2fb9c16
Dohyeon Lee <dlehgus1110@naver.com>
2022-10-03 21:10:26 +0900

    chore(message.js): 게임 결과 메세지 상수 수정 및 삭제
    사용하지 않는 상수를 삭제했습니다.

--- src/constants/message.js
+++ src/constants/message.js
@@ -4,11 +4,6 @@ const ERROR_MSG = {
 	REPEATED_NUM_ERR: `입력에 중복된 숫자가 있습니다. 다시 입력해 주세요!`,
 	RANGE_ERR: `입력 가능한 숫자 범위를 벗어났습니다. 다시 입력해 주세요!`,
 }
-const GAME_MSG = {
-	STLIKE: '스트라이크',
-	BALL: '볼',
-	NOTHING: '낫싱',
-	WIN: '⚾정답을 맞추셨습니다⚾',
-}
+const GAME_WIN_MSG = '⚾정답을 맞추셨습니다⚾'
 
-export { ERROR_MSG, GAME_MSG }
+export { ERROR_MSG, GAME_WIN_MSG }
